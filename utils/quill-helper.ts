f0dd134f65084f3b428220ff39c43a590fe3e128
as <allensnape@qq.com>
2019-10-28 18:20:09 +0800

    .

--- utils/quill-helper.ts
+++ utils/quill-helper.ts
@@ -9,30 +9,34 @@ import {OssService} from '../aliyun/oss.service';
  * @param folder 上传至的路径
  * @param notSignSuffix 是否需要截取OSS签名后缀
  */
-export const onQuillImageUpload = (
+export const onQuillImageUpload = async (
   e: Event, editor: QuillEditorComponent,
   oss: OssService, folder: string,
   notSignSuffix: boolean = true
 ): Promise<void> => {
-  return new Promise<void>((resolve, reject) => {
+  return new Promise<void>(async (resolve, reject) => {
     const fileUploader = e.target as HTMLInputElement;
-    if (fileUploader.files[0]) {
-      const file = fileUploader.files[0];
-      oss.upload(folder, file).subscribe(name => {
-        const range = editor.quillEditor.getSelection();
-        const signedUrl = oss.sign(name, {expires: OssService.DECADE});
-        editor.quillEditor.insertEmbed(
-          range.index, 'image',
-          notSignSuffix ? signedUrl.substring(0, signedUrl.indexOf('?')) : signedUrl
-        );
-        editor.quillEditor.setSelection(range.index + 1, 0);
+    if (fileUploader.files.length) {
+      for (const file of Array.from(fileUploader.files)) {
+        await new Promise(fRes => {
+          oss.upload(folder, file).subscribe(name => {
+            const range = editor.quillEditor.getSelection();
+            const signedUrl = oss.sign(name, {expires: OssService.DECADE});
+            editor.quillEditor.insertEmbed(
+              range.index, 'image',
+              notSignSuffix ? signedUrl.substring(0, signedUrl.indexOf('?')) : signedUrl
+            );
+            editor.quillEditor.setSelection(range.index + 1, 0);
 
-        (e.target as HTMLInputElement).value = '';
+            (e.target as HTMLInputElement).value = '';
 
-        editor.onModelChange(editor.valueGetter(editor.quillEditor, editor.editorElem));
+            editor.onModelChange(editor.valueGetter(editor.quillEditor, editor.editorElem));
 
-        resolve();
-      }, err => reject(err));
+            fRes();
+          }, err => reject(err));
+        });
+      }
+      resolve();
     }
   });
 };
